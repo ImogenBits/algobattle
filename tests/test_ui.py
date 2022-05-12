5ca7de5b47df6337b0f07c0368c86df8981dc781
Imogen <me@imogen.tech>
2022-05-12 13:52:32 +0200

    add _format_obj test

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -1,12 +1,12 @@
 """Tests for the terminal ui."""
-import unittest
+from unittest import TestCase
 from itertools import tee
 
 from algobattle.util import Table
-from algobattle.ui import _format_table
+from algobattle.ui import _format_table, _format_obj
 
 
-class TableTests(unittest.TestCase):
+class TableTests(TestCase):
     """Tests for the Table formatting."""
 
     @classmethod
@@ -50,3 +50,27 @@ class TableTests(unittest.TestCase):
     def test_height_very_small(self):
         formatted = _format_table(self.table, 10000, 3)
         self.check_basic_format(formatted, 10000, 3)
+
+
+
+class UiTests(TestCase):
+    """Tests for other ui module functions."""
+
+    def test_format_obj_table(self):
+        table = Table.from_lists([["frst", "second column", "C"], [2, 3, 4], [1.111111111, None, "9"]])
+        self.assertEqual(_format_obj(table), _format_table(table, 10000, 10000))
+
+    def test_format_obj_mapping(self):
+        mapping = {1: "one", 2: "two", 3: "three" * 50}
+        mapping_formatted = _format_obj(mapping, 20, 5)
+        for line in mapping_formatted:
+            self.assertLessEqual(len(line), 50)
+        self.assertEqual(len(mapping_formatted), 5)
+        self.assertEqual(mapping_formatted[3][:7], " " * 7)
+
+    def test_format_obj_string(self):
+        string_formatted = _format_obj("test" * 50, 16, 5)
+        self.assertEqual(string_formatted, ["test" * 4] * 5)
+
+    def test_format_obj_none(self):
+        self.assertEqual(_format_obj(None), [])
