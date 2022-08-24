8e96be6843f784836df83eddb2c8c16b75278197
Imogen <me@imogen.tech>
2022-08-24 17:10:21 +0200

    add docker to dependencies

--- algobattle/battle.py
+++ algobattle/battle.py
@@ -15,7 +15,7 @@ from algobattle.match import Match
 from algobattle.team import Team
 from algobattle.util import import_problem_from_path, initialize_wrapper
 from algobattle.ui import Ui
-from algobattle.docker import DockerError, measure_runtime_overhead
+from algobattle.docker_wrapper import DockerError, measure_runtime_overhead
 
 
 def setup_logging(logging_path: Path, verbose_logging: bool, silent: bool):
